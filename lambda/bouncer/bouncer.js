7fc12ff1e88a2964d4c180dafeed7e0c5b2f557b
Ben Cooke <btc@amazon.com>
2023-08-23 08:57:42 -0500

    promisify and clean up lambda functions

--- lambda/bouncer/bouncer.js
+++ lambda/bouncer/bouncer.js
@@ -7,145 +7,88 @@ function is attached as a rule engine action to the registration topic
 Saws/events/certificates/registered/<caCertificateID>
 **/
 
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 const secondsSinceEpoch = Math.round(Date.now() / 1000).toString().trim();
 const { X509Certificate } = require('node:crypto');
 
-exports.handler = function(event, context, callback) {
-    
-//    console.log(event);
+exports.handler = async function(event, context) {
+    const region = process.env.AWS_REGION.toString().trim();
+    const accountId = event.awsAccountId.toString().trim();
 
-    var region = process.env.AWS_REGION.toString().trim();
-    var accountId = event.awsAccountId.toString().trim();
-
-    var iot = new AWS.Iot({'region': region, apiVersion: '2015-05-28'});
-    var certificateId = event.certificateId.toString().trim();
+    const iot = new AWS.Iot({'region': region, apiVersion: '2015-05-28'});
+    const certificateId = event.certificateId.toString().trim();
 
     // DynamoDB service object
-    var ddbClient = new AWS.DynamoDB.DocumentClient;
+    const ddbClient = new AWS.DynamoDB.DocumentClient;
 
-    var topicName = `lobby`;
-    var certificateARN = `arn:aws:iot:${region}:${accountId}:cert/${certificateId}`;
+    const certificateARN = `arn:aws:iot:${region}:${accountId}:cert/${certificateId}`;
 
     // table and policy names passed in via CF vars
-    var policyName = process.env.IOT_POLICY;
-    var table = process.env.DEVICE_LEDGER_TABLE;
+    const policyName = process.env.IOT_POLICY;
+    const table = process.env.DEVICE_LEDGER_TABLE;
 
-    var certthingname = "notset";
+    let certthingname = "notset";
 
-    /*
-    Create a policy
-    */
-        //Policy created by cloudformation
+    try {
+        await iot.attachPrincipalPolicy({
+            policyName: policyName,
+            principal: certificateARN
+        }).promise();
 
-    /*
-    Attach the policy to the certificate to enable
-    */
-    iot.attachPrincipalPolicy({
-        policyName: policyName,
-        principal: certificateARN
-    }, (err, data) => {
-        //Ignore if the policy is already attached
-        if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-            console.log(err);
-            callback(err, data);
+        const certData = await iot.describeCertificate({
+            certificateId: certificateId
+        }).promise();
+
+        const pem = certData.certificateDescription.certificatePem;
+        const x509 = new X509Certificate(pem);
+        const subject = x509.subject;
+        certthingname = subject.substr((subject.search("CN="))+3, (subject.length)).trim();
+
+        await iot.createThing({
+            thingName: certthingname,
+            attributePayload: {
+                attributes: {
+                    'type': '42',
+                },
+                merge: true
+            } 
+        }).promise();
+
+        await iot.attachThingPrincipal({
+            principal: certificateARN,
+            thingName: certthingname
+        }).promise();
+
+        await iot.updateCertificate({
+            certificateId: certificateId,
+            newStatus: 'ACTIVE'
+        }).promise();
+
+        const params = {
+            TableName: table,
+            Key:{
+                'certid' : certificateId
+            },
+            UpdateExpression: 'set devicecert = :cert, devicename = :dn, config = :c, ts = :ts, devstate = :s, doormancount = :cnt',
+            ExpressionAttributeValues: {
+            ':cert' : pem,
+            ':dn' : certthingname,
+            ':ts' : secondsSinceEpoch,
+            ':s' : 'knocking',
+            ':c' : 'default config',
+            ':cnt' : 0
+            }
+        };
+
+        await ddbClient.update(params).promise();
+
+        return "Success, created thing, created policy, attached policy and activated the certificate " + certificateId;
+    } catch (err) {
+        if (err.code === 'ResourceAlreadyExistsException') {
+            // Ignore if the policy is already attached
             return;
         }
-        // console.log(data);
-        
-        /*
-        get thing name from cert CN and register it
-        */
-        iot.describeCertificate({
-            certificateId: certificateId
-        }, (err,data) => {
-            if (err) console.log(err, err.stack); // an error occurred
-            else     {
-                // console.log(data);           // successful response
-    
-                var pem = data.certificateDescription.certificatePem;
-                // console.log("Here is the PEM!");
-                // console.log(pem);
-                const x509 = new X509Certificate(pem);
-                
-                const subject = x509.subject;
-                // console.log(subject);
-                    
-                certthingname = subject.substr((subject.search("CN="))+3, (subject.length)).trim();
-                // console.log('thing name from cert:',certthingname);
-                // console.log('Now register the name!');
-                
-                //register thing name
-                iot.createThing({
-                    thingName: certthingname,
-                    attributePayload: {
-                        attributes: {
-                            'type': '42',
-                            /* '<AttributeName>': ... */
-                        },
-                        merge: true
-                        } 
-                }, (err,data) => {
-                    if (err) console.log(err, err.stack); // an error occurred
-                    else     {
-                        // console.log("Thingname created successfully! ARN: ", data.thingArn);
-                        //attach thing to cert
-                        
-                        iot.attachThingPrincipal({
-                            principal: certificateARN,
-                            thingName: certthingname
-                        }, (err, data) => {
-                            if (err) console.log(err, err.stack); // an error occurred
-                            else     {
-                                // console.log("Cert attached to Thing!");
-                                
-                                /*
-                                Activate the certificate. Optionally, you can have your custom Certificate Revocation List (CRL) check
-                                logic here and ACTIVATE the certificate only if it is not in the CRL. Revoke the certificate if it is in the CRL
-                                */
-                                iot.updateCertificate({
-                                    certificateId: certificateId,
-                                    newStatus: 'ACTIVE'
-                                }, (err, data) => {
-                                    if (err) {
-                                        console.log(err, err.stack); 
-                                        callback(err, data);
-                                    }
-                                    else {
-                                        // console.log(data);   
-                                        
-                                        var params = {
-                                            TableName: table,
-                                            Key:{
-                                                'certid' : certificateId
-                                            },
-                                            UpdateExpression: 'set devicecert = :cert, devicename = :dn, config = :c, ts = :ts, devstate = :s, doormancount = :cnt',
-                                            ExpressionAttributeValues: {
-                                            ':cert' : pem,
-                                            ':dn' : certthingname,
-                                            ':ts' : secondsSinceEpoch,
-                                            ':s' : 'knocking',
-                                            ':c' : 'default config',
-                                            ':cnt' : 0
-                                            }
-                                        };
-                                        
-                                        ddbClient.update(params, function(err, data) {
-                                            if (err) {
-                                                console.log("Unable to update item: " + "\n" + JSON.stringify(err, undefined, 2));
-                                            } else {
-                                                console.log("Update to device lobby ledger entry succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
-                                            }
-                                        });
-                                        
-                                        callback(null, "Success, created thing, created policy, attached policy and activated the certificate " + certificateId);
-                                    }
-                                });
-                            }
-                        });
-                    } 
-                });
-            }
-        });
-    });
+        console.log(err);
+        throw err;
+    }
 };
--- lambda/commissioner/commissioner.js
+++ lambda/commissioner/commissioner.js
@@ -6,173 +6,94 @@ If a completed record is seen that includes the device identity and a target acc
 will attempt to register the thing in the target account/region.
 **/
 
-// console.log('Loading function');
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
-function CommissionTarget (tregion, taccount, creds, tobj) {
-    
-    // console.log("Attempting to setup device in: " + tregion);
-    // console.log("Using these creds: " + JSON.stringify(creds));
-    
-    // console.log("Thing object: " + JSON.stringify(tobj));
-    
-    var iot = new AWS.Iot({'region': tregion, credentials: creds, apiVersion: '2015-05-28'});
-    
-    var certpem = tobj.cert.toString().trim();
-    var policyname = tobj.name.toString().trim() + tobj.certId.toString().trim();
-    
-    // console.log(certpem);
-    // console.log(policyname);
-    
-    iot.registerCertificateWithoutCA({
-        certificatePem: certpem
-    }, (err, data) => {
-        //Ignore if cert already exists
-        if (err && (err.code !== 'ResourceAlreadyExistsException')) {
-            console.log(err, err.stack); // an error occurred
-            return;
+// Helper function to trim strings
+const trimString = (str) => str.toString().trim();
+
+async function CommissionTarget (tregion, taccount, creds, tobj) {
+    const iot = new AWS.Iot({'region': tregion, credentials: creds, apiVersion: '2015-05-28'});
+    const certpem = trimString(tobj.cert);
+    const policyname = `${trimString(tobj.name)}${trimString(tobj.certId)}`;
+
+    try {
+        const data = await iot.registerCertificateWithoutCA({
+            certificatePem: certpem
+        }).promise();
+
+        let certificateARN;
+        if(data.code == 'ResourceAlreadyExistsException') {
+            certificateARN = `arn:aws:iot:${tregion}:${taccount}:cert/${trimString(tobj.certId)}`;
+        } else {
+            certificateARN = trimString(data.certificateArn);
         }
-        else {
-            // console.log(err);
-            // console.log(data);           // successful response
-            var certificateARN;
-            if(err.code == 'ResourceAlreadyExistsException') {
-                certificateARN = "arn:aws:iot:" + tregion + ":" + taccount + ":cert/" + tobj.certId.toString().trim();
-            } else {
-                certificateARN = data.certificateArn.toString().trim();
+
+        await iot.createPolicy({
+            policyDocument: JSON.stringify(tobj.policy),
+            policyName: `${trimString(tobj.name)}${trimString(tobj.certId)}`,
+        }).promise();
+
+        await iot.attachPrincipalPolicy({
+            policyName: policyname,
+            principal: certificateARN
+        }).promise();
+
+        await iot.createThing({
+            thingName: trimString(tobj.name)
+        }).promise();
+
+        await iot.attachThingPrincipal({
+            principal: certificateARN,
+            thingName: trimString(tobj.name)
+        }).promise();
+
+        await iot.updateCertificate({
+            certificateId: trimString(tobj.certId),
+            newStatus: 'ACTIVE'
+        }).promise();
+
+        const data = await iot.describeEndpoint({
+            endpointType: 'iot:Data-ATS'
+        }).promise();
+
+        const commissionedep = trimString(data.endpointAddress);
+
+        const ddbClient = new AWS.DynamoDB.DocumentClient;
+        const table = "devicelobby-ledger";
+        await ddbClient.update({
+            TableName: table,
+            Key:{
+              'certid' : trimString(tobj.certId)
+            },
+            UpdateExpression: 'set iotep = :ep',
+            ExpressionAttributeValues: {
+            ':ep' : commissionedep
             }
-            // console.log("HERE IS THE CERTARN: " + certificateARN);
-            iot.createPolicy({
-                policyDocument: JSON.stringify(tobj.policy),
-                policyName: tobj.name.toString().trim() + tobj.certId.toString().trim(),
-            }, (err, data) => {
-                //Ignore if the policy already exists
-                if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-                    console.log(err);
-                    return;
-                } else {
-                    // console.log(err);
-                    // console.log(data);           // successful response
-                    iot.attachPrincipalPolicy({
-                        policyName: policyname,
-                        principal: certificateARN
-                    }, (err, data) => {
-                        //Ignore if the policy is already attached
-                        if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-                            console.log(err);
-                            return;
-                        } else {
-                            iot.createThing({
-                                thingName: tobj.name.toString().trim()
-                            }, (err,data) => {
-                                //Ignore if thing already exists
-                                if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-                                    console.log(err);
-                                    return;
-                                } else {
-                                    // console.log(err);
-                                    // console.log(data);
-                                    iot.attachThingPrincipal({
-                                        principal: certificateARN,
-                                        thingName: tobj.name.toString().trim()
-                                    }, (err, data) => {
-                                        //Ignore if the thing is already attached
-                                        if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-                                            console.log(err);
-                                            return;
-                                        } else {
-                                            // console.log(err);
-                                            // console.log("Cert attached to Thing!");
-                                            iot.updateCertificate({
-                                                certificateId: tobj.certId.toString().trim(),
-                                                newStatus: 'ACTIVE'
-                                            }, (err, data) => {
-                                                 //Ignore if cert is already attached
-                                                if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-                                                    console.log(err);
-                                                    return;
-                                                } else {
-                                                    // console.log(err);
-                                                    // console.log(data);   
-                                                    console.log("Success, created thing, created policy, attached policy and activated the certificate in target account/region");
-                                                    //get the iotep to set in the table
-                                                    
-                                                    iot.describeEndpoint({
-                                                        endpointType: 'iot:Data-ATS'
-                                                    }, (err, data) => {
-                                                        if (err) {
-                                                            console.log(err, err.stack); // an error occurred
-                                                            return;
-                                                        } else {
-                                                            // console.log(err);
-                                                            // console.log(data);
-                                                            var commissionedep = data.endpointAddress.toString().trim()
-                                                            console.log("IoT EP of target region is: " + commissionedep);           // successful response
-                                                            //now write the new iot-ep back to ddb indicating it is ready.
-                                                            var ddbClient = new AWS.DynamoDB.DocumentClient;
-                                                            var table = "devicelobby-ledger";
-                                                            ddbClient.update({
-                                                                TableName: table,
-                                                                Key:{
-                                                                  'certid' : tobj.certId.toString().trim()
-                                                                },
-                                                                UpdateExpression: 'set iotep = :ep',
-                                                                ExpressionAttributeValues: {
-                                                                ':ep' : commissionedep
-                                                                }
-                                                            }, (err, data) => {
-                                                                if (err) {
-                                                                    console.log(err);
-                                                                } else {
-                                                                    console.log(data);
-                                                                    console.log(tobj.name.toString().trim() + " registered in target account and ledge table updated with target iotep: " + commissionedep);
-                                                                }
-                                                            });
-                                                        }
-                                                    });
-                                                }
-                                            });
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-            });
+        }).promise();
+
+    } catch (err) {
+        if (err.code !== 'ResourceAlreadyExistsException') {
+            console.error(err);
+            throw err;
         }
-            
-    });
- 
-    return;   
+    }
 }
 
-exports.handler = function(event, context, callback) {
-    // console.log(JSON.stringify(event, null, 2));
-    var sts = new AWS.STS({apiVersion: '2011-06-15'});
+exports.handler = async function(event, context) {
+    const sts = new AWS.STS({apiVersion: '2011-06-15'});
 
-    event.Records.forEach(function(record) {
-        // console.log(record.eventID);
-        // console.log(record.eventName);
-        //only check record modifications
+    for (let record of event.Records) {
         if(record.eventName == 'MODIFY') {
             console.log('DynamoDB Record: %j', record.dynamodb);
-            
-            //there are two requirements for being able to commision a new account:
-            //  1) a new target account/region to indicate where to commission
-            //  2) the device cert... which is only available after the device connnects to the lobby
-            
-           // iotep string will be fetched later
-           
-           if ((record.dynamodb.NewImage.hasOwnProperty("targetaccount")) && (record.dynamodb.NewImage.hasOwnProperty("targetregion")) && record.dynamodb.NewImage.hasOwnProperty("devicecert")) {
-               
-                var region = record.dynamodb.NewImage.targetregion.S.toString().trim();
-                var accountId = record.dynamodb.NewImage.targetaccount.S.toString().trim();
-                var role = "arn:aws:iam::" + accountId + ":role/lobby_commissioner_role";
-                
-                var roleCreds;
 
-                var thingpolicy = {
+            if ((record.dynamodb.NewImage.hasOwnProperty("targetaccount")) && (record.dynamodb.NewImage.hasOwnProperty("targetregion")) && record.dynamodb.NewImage.hasOwnProperty("devicecert")) {
+                const region = trimString(record.dynamodb.NewImage.targetregion.S);
+                const accountId = trimString(record.dynamodb.NewImage.targetaccount.S);
+                const role = `arn:aws:iam::${accountId}:role/lobby_commissioner_role`;
+
+                let roleCreds;
+
+                const thingpolicy = {
                     "Version": "2012-10-17",
                     "Statement": [
                         {
@@ -180,80 +101,48 @@ exports.handler = function(event, context, callback) {
                             "Action": [
                                 "iot:Connect"
                             ],
-                            "Resource": "arn:aws:iot:" + region + ":" + accountId + ":client/*"
+                            "Resource": `arn:aws:iot:${region}:${accountId}:client/*`
                         },
                         {
                             "Effect": "Allow",
                             "Action": [
                                 "iot:Publish"
                             ],
-                            "Resource": "arn:aws:iot:" + region + ":" + accountId + ":topic/$aws/rules/lobby/"
+                            "Resource": `arn:aws:iot:${region}:${accountId}:topic/$aws/rules/lobby/`
                         },
-                                                {
+                        {
                             "Effect": "Allow",
                             "Action": [
                                 "iot:Publish"
                             ],
-                            "Resource": "arn:aws:iot:" + region + ":" + accountId + ":topic/app/*"
+                            "Resource": `arn:aws:iot:${region}:${accountId}:topic/app/*`
                         },
                         {
                             "Effect": "Allow",
                             "Action": [
                                 "iot:Receive"
                             ],
-                            "Resource": "arn:aws:iot:" + region + ":" + accountId + ":topic/lobby*"
+                            "Resource": `arn:aws:iot:${region}:${accountId}:topic/lobby*`
                         },
                         {
                             "Effect": "Allow",
                             "Action": [
                                 "iot:Subscribe",
                             ],
-                            "Resource": "arn:aws:iot:" + region + ":" + accountId + ":topicfilter/lobby*"
+                            "Resource": `arn:aws:iot:${region}:${accountId}:topicfilter/lobby*`
                         }
                     ]
                 };
-                
-                var thingobj = {
-                    name: record.dynamodb.NewImage.devicename.S.toString().trim(),
-                    cert: record.dynamodb.NewImage.devicecert.S.toString().trim(),
-                    certId: record.dynamodb.NewImage.certid.S.toString().trim(),
-                    policy: thingpolicy
-                }
-                
-                // if(record.dynamodb.NewImage.iotep.S != record.dynamodb.NewImage.iotep.S) {
-                    // console.log("New iotep written... check if cert is present");
-                    // now check to make sure the cert is present before we commission
-                    if(record.dynamodb.NewImage.devicecert.S.startsWith("-----BEGIN CERTIFICATE-----")) {
-                        console.log("EP1: device cert exists...  commissioning target account.");
-                        console.log("commissioning with this role:    " + role);
-                        var params = {
-                            ExternalId: "123ABC", 
-                            RoleArn: role, 
-                            RoleSessionName: "CommissionAssumeRoleSession"
-                         };
-                         
-                        sts.assumeRole(params, function(err, data) {
-                            if (err) console.log(err, err.stack);
-                            else{
-                                roleCreds = {accessKeyId: data.Credentials.AccessKeyId,
-                                             secretAccessKey: data.Credentials.SecretAccessKey,
-                                             sessionToken: data.Credentials.SessionToken};
-                                CommissionTarget(region, accountId, roleCreds, thingobj);
-                            }
-                        });
-                        return;
-                        
-                    } else {
-                        //console.log("cert is not present... do nothing until device actually connects so the cert can be captured");
-                        return;
-                    }
-                // }
-           } else { 
-            //console.log("target account, region and/or cert not found in record"); 
-            return; 
-            }
 
+                const thingobj = {
+                    name: trimString(record.dynamodb.NewImage.devicename.S),
+                    cert: trimString(record.dynamodb.NewImage.devicecert.S),
+                    certId: trimString(record.dynamodb.NewImage.certid.S),
+                    policy: thingpolicy
+                };
+
+                await CommissionTarget(region, accountId, roleCreds, thingobj);
+            }
         }
-    });
-    callback(null, "message");
+    }
 };
